ec450ec1185e03187f8c500877c46c4df67d6880
millyjain09 <mishradeepu400@gmail.com>
2025-12-14 13:54:35 +0530

    first commit

--- frontend/src/Context/AuthContext.jsx
+++ frontend/src/Context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createContext } from 'react'
 export const AuthDataContext=createContext();
 function AuthContext({children}) {
-    let serverUrl="http://localhost:8000"
+    let serverUrl="https://shynex.onrender.com"
     let value={
         serverUrl
     }
