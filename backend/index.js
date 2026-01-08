e656cd50666ceccf3f0cf49e3b9fac35623ea88d
millyjain09 <mishradeepu400@gmail.com>
2026-01-08 22:15:03 +0530

    changes done

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ dotenv.config();
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-pro"
+  model: "gemini-1.5-flash"
 });
 
 
